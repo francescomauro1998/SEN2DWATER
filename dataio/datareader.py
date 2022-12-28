b8c1cf2301efd314e217b913f3f682e1ed76b9f1
alessandrosebastianelli <alessandroseba1995@gmail.com>
2022-12-28 15:43:38 +0100

    split images into patches

--- dataio/datareader.py
+++ dataio/datareader.py
@@ -57,6 +57,38 @@ class datareader:
             print('!!! File can not be opened, format not supported !!!')
             
         return data, metadata
+   
+    @staticmethod
+    def save(image, path, meta):
+        '''
+            Save an image and its metadata given its path
+            Inputs:
+                - image: the image to be saved
+                - path: position of the image
+                - meta: metadata for the image to be saved
+        '''
+
+        RASTERIO_EXTENSIONS   = ['.tif', '.tiff']
+        MATPLOTLIB_EXTENSIONS = ['.png', '.jpg', 'jpeg']
+
+        if any(frmt in path for frmt in RASTERIO_EXTENSIONS):
+
+            if meta!=None:
+                meta.update({'driver':'GTiff',
+                            'width':image.shape[0],
+                            'height':image.shape[1],
+                            'count':image.shape[2],
+                            'dtype':'float64'})
+
+            with rasterio.open(fp=path, mode='w',**meta) as dst:
+                for count in range(image.shape[2]):
+                    dst.write(image[:,:,count], count+1)
+
+        elif any(frmt in path for frmt in MATPLOTLIB_EXTENSIONS):
+            plt.imsave(path, image)
+
+        else:
+            print('[!] File cannot be saved, format not supported!')
 
     @staticmethod
     def generator(dataset, batch_size, t_len, img_shape, normalize=True):
